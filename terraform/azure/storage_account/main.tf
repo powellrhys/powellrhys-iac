d52ea697f8970443095dbc5d3a9e3da78942ddd7
rhys powell <rhys.powell2@btinternet.com>
2025-05-19 19:21:07 +0100

    apply step added

--- terraform/azure/storage_account/main.tf
+++ terraform/azure/storage_account/main.tf
@@ -3,11 +3,13 @@ resource "azurerm_resource_group" "storage_account_rg" {
   location = var.storage_account_location
 }
 
-
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.storage_account_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  kind                     = "StorageV2"
+  access_tier              = "Cool"
+  enable_https_traffic_only = true      
 }
