a35e8d6c333f04d1d4e23d900bfaf76223933945
rhys powell <rhys.powell2@btinternet.com>
2025-05-19 19:11:40 +0100

    account name change

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "storage_account" {
   source = "./terraform/azure/storage_account"
 
   resource_group_name                 = "powellrhys-project-data-rg"
-  storage_account_name                = "powellrhys-project-data"
+  storage_account_name                = "powellrhysdata"
   storage_account_location            = "westeurope"
 }
