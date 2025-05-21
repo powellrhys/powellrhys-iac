2a61924efbf8a986d910939ec535f926d82cabe0
rhys powell <rhys.powell2@btinternet.com>
2025-05-21 20:43:19 +0100

    storage account depends on added

--- terraform/azure/storage_account/main.tf
+++ terraform/azure/storage_account/main.tf
@@ -5,12 +5,16 @@ resource "azurerm_resource_group" "storage_account_rg" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.storage_account_rg.name
   location                 = var.storage_account_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   access_tier              = "Cool"
-  enable_https_traffic_only = true      
+  enable_https_traffic_only = true
+
+  depends_on = [
+    azurerm_resource_group.storage_account_rg
+  ]
 }
 
 resource "azurerm_storage_container" "containers" {
@@ -18,4 +22,8 @@ resource "azurerm_storage_container" "containers" {
   name                  = each.value
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
+
+  depends_on = [
+    azurerm_storage_account.storage_account
+  ]
 }
