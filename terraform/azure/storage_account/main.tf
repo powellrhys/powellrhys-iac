d287ab5cda4e41000f804af056f4f19f3411ef47
rhys powell <rhys.powell2@btinternet.com>
2025-06-20 08:29:28 +0100

    depends on duplication removed

--- terraform/azure/storage_account/main.tf
+++ terraform/azure/storage_account/main.tf
@@ -20,8 +20,4 @@ resource "azurerm_storage_container" "containers" {
     name                  = each.value
     storage_account_name  = azurerm_storage_account.storage_account.name
     container_access_type = "private"
-
-    depends_on = [
-      azurerm_storage_account.storage_account
-    ]
 }
