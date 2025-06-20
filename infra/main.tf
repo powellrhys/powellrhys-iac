c2d3f8a8dbc8594a6218fbcefe18f7500e83018f
rhys powell <rhys.powell2@btinternet.com>
2025-06-20 08:33:45 +0100

    trigger fix + tf validation changes

--- infra/main.tf
+++ infra/main.tf
@@ -13,8 +13,8 @@ module "storage_account" {
 module "app_service_plan" {
   source = "../terraform/azure/app_service_plan"
 
-    resource_group_name      = "powellrhys-app-service-plan-rg"
-    storage_account_name     = "applications"
+    resource_group_name       = "powellrhys-app-service-plan-rg"
+    app_service_plan_name     = "applications"
     app_service_plan_location = var.location
     os_type = "Linux"
 }
