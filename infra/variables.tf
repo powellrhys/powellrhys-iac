58c916fc53478739de9dd1b90183d0ebef68cb31
rhys powell <rhys.powell2@btinternet.com>
2025-07-04 10:50:26 +0100

    debugging app settings issue

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,9 @@ variable location {
   default     = "westeurope"
   description = "Location within azure"
 }
+
+variable app_service_app_settings {
+  type        = string
+  default     = ""
+  description = "Azure app service app settings"
+}
